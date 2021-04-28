0b8472fa1fa06847cd74a781ec43a7a9c7891526
167196 <167196@ust-global.com>
2021-04-28 07:22:09 +0000

    angular frontend

---
+++ src/app/book/bookcard/book-card.component.ts
@@ -0,0 +1,14 @@
+import { templateJitUrl } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-card',
+  //template: '<h1>Iam a Card</h1>',
+  //styles: ['h1{font-weight: normal}'],
+  templateUrl: 'book-card.component.html',
+  styleUrls: ['book-card.component.css']
+
+})
+export class BookCardComponent {
+
+}
